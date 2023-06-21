3ac08e545cefeba0a17ac40c4c6b8fa8543ad819
Javi <jcl.199782@gmail.com>
2023-06-21 20:40:00 +0200

    [00.00.01] Primeras clases

--- src/test/java/es/lithium/barquitos/init/BarquitosTest.java
+++ src/test/java/es/lithium/barquitos/init/BarquitosTest.java
@@ -1,6 +1,5 @@
 package es.lithium.barquitos.init;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class BarquitosTest {
